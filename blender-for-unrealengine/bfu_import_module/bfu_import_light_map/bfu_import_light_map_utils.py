ea6080d5307b4de41f2a7df90fbc4c3b5b53400b
BleuRaven <xavierloux.loux@gmail.com>
2025-06-10 15:19:27 +0200

    Fix Unreal importer to set the wrong value for light map "generate_light_map_uvs" -> "generate_lightmap_u_vs"

--- blender-for-unrealengine/bfu_import_module/bfu_import_light_map/bfu_import_light_map_utils.py
+++ blender-for-unrealengine/bfu_import_module/bfu_import_light_map/bfu_import_light_map_utils.py
@@ -16,7 +16,6 @@
 #
 # ======================= END GPL LICENSE BLOCK =============================
 
-from typing import Optional
 from .. import import_module_unreal_utils
 from .. import import_module_tasks_class
 from .. import import_module_utils
@@ -64,14 +63,15 @@ def apply_asset_settings(itask: import_module_tasks_class.ImportTaks, asset_addi
                 build_settings.min_lightmap_resolution = asset_additional_data["light_map_resolution"]
                 unreal.EditorStaticMeshLibrary.set_lod_build_settings(static_mesh, 0, build_settings)
 
-    if itask.use_interchange:
-        if "generate_light_map_uvs" in asset_additional_data:
-            mesh_pipeline = static_mesh.get_editor_property('asset_import_data').get_pipelines()[0].get_editor_property('mesh_pipeline')
-            mesh_pipeline.set_editor_property('generate_light_map_uvs', asset_additional_data["generate_light_map_uvs"])  # Import data
-            unreal.EditorStaticMeshLibrary.set_generate_lightmap_uv(static_mesh, asset_additional_data["generate_light_map_uvs"])  # Build settings at lod
+    asset_import_data = static_mesh.get_editor_property('asset_import_data')
+    if "generate_light_map_uvs" in asset_additional_data:
+        generate_light_map_uvs: bool = asset_additional_data["generate_light_map_uvs"]
+        if itask.use_interchange:
+            mesh_pipeline = asset_import_data.get_pipelines()[0].get_editor_property('mesh_pipeline')
+            mesh_pipeline.set_editor_property('generate_lightmap_u_vs', generate_light_map_uvs)  # Import data
+            unreal.EditorStaticMeshLibrary.set_generate_lightmap_uv(static_mesh, generate_light_map_uvs)  # Build settings at lod
 
-    else:
-        asset_import_data = static_mesh.get_editor_property('asset_import_data')
-        if "generate_light_map_uvs" in asset_additional_data:
-            asset_import_data.set_editor_property('generate_light_map_uvs', asset_additional_data["generate_light_map_uvs"])  # Import data
-            unreal.EditorStaticMeshLibrary.set_generate_lightmap_uv(static_mesh, asset_additional_data["generate_light_map_uvs"])  # Build settings at lod
+        else:
+            generate_light_map_uvs: bool = asset_additional_data["generate_light_map_uvs"]
+            asset_import_data.set_editor_property('generate_lightmap_u_vs', generate_light_map_uvs)  # Import data
+            unreal.EditorStaticMeshLibrary.set_generate_lightmap_uv(static_mesh, generate_light_map_uvs)  # Build settings at lod
