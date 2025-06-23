0567cf9ba2449f7d1476c60c3182109b00df4f96
BleuRaven <Xavierloux.loux@gmail.com>
2025-06-23 20:49:14 +0200

    Clear debug options

--- blender-for-unrealengine/bfu_import_module/config.py
+++ blender-for-unrealengine/bfu_import_module/config.py
@@ -20,7 +20,7 @@ select_assets_after_import: bool = True
 save_assets_after_import: bool = True
 
 # DEBUG
-automated_import_tasks: bool = False # "True" by default. You can use "False" for debug.
+automated_import_tasks: bool = True # "True" by default. You can use "False" for debug.
 # automated_import_tasks when False will show up the import task option popup.
 # This will in the most of cases override the optionss set in the import script. So use that for debug is a bad idea.
-print_debug_steps: bool = True # "False" by default. You can use "True" for debug.
+print_debug_steps: bool = False # "False" by default. You can use "True" for debug.
